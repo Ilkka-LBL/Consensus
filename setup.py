3ba8ef35cda9918dafc177a1bf086279f6b7e3ea
Ilkka-LBL <ilkka.sipila@lewisham.gov.uk>
2024-10-24 15:45:42 +0100

    Docs cleanup

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ all_files = extra_files + config
 
 setup(
     name='Consensus',
-    version='1.0.0',
+    version='1.0.1',
     author='Ilkka Sipila',
     author_email='ilkka.sipila@lewisham.gov.uk',
     packages=find_packages(),
