b63ba77290d50a660948ab703b11aa6cebd50865
Ilkka-LBL <ilkka.sipila@lewisham.gov.uk>
2024-10-24 09:46:35 +0100

    Merge branch to main to test PyPI

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ all_files = extra_files + config
 
 setup(
     name='Consensus',
-    version='1.0.1',
+    version='1.0.0',
     author='Ilkka Sipila',
     author_email='ilkka.sipila@lewisham.gov.uk',
     packages=find_packages(),
